ae6e7b71cab7c5b8b49d7902d9a042dafbfc526f
chrling <chr.ling@alumni.utoronto.ca>
2019-01-29 19:50:46 -0500

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -3,12 +3,7 @@ module lab_2b(
 	input [8:0] SW, // [8] cin, [7:4] A, [3:0] B
 	);
 	
-	wire fa0_cout, fa1_cout, fa2_cout;
-	
-	full_adder fa0(fa0_cout, LEDR[0], SW[4], SW[0], SW[8]);
-	full_adder fa1(fa1_cout, LEDR[1], SW[5], SW[1], fa0_cout);
-	full_adder fa2(fa2_cout, LEDR[2], SW[6], SW[2], fa1_cout);
-	full_adder fa3(LEDR[9], LEDR[3], SW[7], SW[3], fa2_cout);
+	full_adder_4bits(LEDR[9], LEDR[3:0], SW[7:4], SW[3:0], SW[8]);
 	
 endmodule
 
@@ -19,3 +14,19 @@ module full_adder(cout, sum, a, b, cin);
 	assign cout = a & b | a & cin | b & cin;
 	assign sum = a ^ b ^ cin;
 endmodule
+
+module full_adder_4bits(cout, sum, a, b, cin);
+	output cout;
+	output [3:0] sum;
+	input [3:0] a, b;
+	input cin;
+	
+	wire fa0_cout, fa1_cout, fa2_cout;
+	
+	full_adder fa0(fa0_cout, sum[0], a[0], b[0], cin);
+	full_adder fa1(fa1_cout, sum[1], a[1], b[1], fa0_cout);
+	full_adder fa2(fa2_cout, sum[2], a[2], b[2], fa1_cout);
+	full_adder fa3(cout, sum[3], a[3], b[3], fa2_cout);
+	
+endmodule
+	
