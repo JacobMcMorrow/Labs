fdd58fed72c6c16ca0cc57502bc4cb696c3e46e4
JacobMcMorrow <jacobmcmorrow@rogers.com>
2019-02-03 19:50:08 -0500

    Added rigerster to lab_3.

--- lab_3/lab_3.v
+++ lab_3/lab_3.v
@@ -1,35 +1,55 @@
 module lab_3(
+	output [7:0] LEDR, // binary representation of ALUout[7:0]
+	output [6:0] HEX0, // hex representation of Data
+	output [6:0] HEX4, // display least significant bit of register
+	output [6:0] HEX5, // display most significant bit of register
 	input [9:0] SW,
-	output [9:0] LEDR // will only use 0
+	input [3:0] KEY
 	);
 	
-	alu my_alu(LEDR[0], SW[6:0], SW[9:7]);
+	wire [7:0] ALUout, REGout;
+	
+	alu my_alu(ALUout, SW[7:4], SW[3:0] KEY[3:1]);
+	register my_register(REGout, KEY0, SW[9], ALUout);
+	
+	// LEDR binary display for ALUout
+	assign LEDR = ALUout;
 endmodule
 
-module alu(
-	output reg out,
-	input[6:0] SW,
-	input[2:0] sel
-	);
+module alu(out, A, B, sel);
+	output reg [7:0] out;
+	input[3:0] A, B;
+	input[2:0] sel;
+
+	wire [7:0] fa_out;
 	
 	always @(*)
 	begin
 		case (sel)
-			// A + B full adder
-			3'b000: out = SW[0];
+			// A + B lab 2 full adder
+			3'b000: begin
+						full_adder_4bits(fa_out[4], fa_out[3:0], A, B, 0);
+						out = fa_out;
+					end
 			// A + B verilog '+'
-			3'b001: out = SW[1];
+			3'b001: out = A + B;
 			// A XOR B lower four bits A OR B upper four bits
-			3'b010: out = SW[2];
-			// 
-			3'b011: out = SW[3];
-			// 
-			3'b100: out = SW[4];
-			// 
-			3'b101: out = SW[5];
-			// 
-			3'b110: out = SW[6];
-			default: out = 1'b0; // default to 1 bit 0
+			3'b010: begin
+						out[7:4] = A | B;
+						out[3:0] = A ^ B;
+					end
+			// output 1 if there is a 1 in the 2 inputs
+			3'b011: out = |{A, B};
+			// output 1 if there is a 1 in the 2 inputs
+			3'b100: out = &{A, B};
+			// Left shift B by A bits
+			3'b101: out = B << A;
+			// Right shift B by A bits 
+			3'b110: out = B >> A;
+			// A x B verilog '*'
+			3'b111: out = A * B;
+			// default to 8 bit 0
+			default: out = 8'b00000000;
 		endcase
 	end
 endmodule
--- lab_3/register.v
+++ lab_3/register.v
@@ -1,11 +1,11 @@
-module register(q, clk, reset_n, d);
-	input clk, reset_n;
+module register(q, clk, reset_b, d);
+	input clk, reset_b;
 	input [7:0] d;
 	output reg [7:0] q;
 	
 	always @(posedge clk)
 	begin 
-		if (reset_n == 1'b0)
+		if (reset_b == 1'b0)
 			q <= 0;
 		else
 			q <= d;
